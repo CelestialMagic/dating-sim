26083058d7824a3f4c17a5e94092c0811ae92632
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-04-18 11:22:53 -0700

    Edited Character Select to Eventually Hide Certain Players

--- Horror Dating Sim/Assets/Scripts/CharacterSelection/CharacterSelect.cs
+++ Horror Dating Sim/Assets/Scripts/CharacterSelection/CharacterSelect.cs
@@ -5,14 +5,35 @@ public class CharacterSelect : MonoBehaviour
     [SerializeField]
     private string characterName; 
 
-
+    [SerializeField]
+    private CharacterName internalName; 
     private PlayerData playerData;
     private SaveDataJSON saveDataJSON;
 
+
+    public enum CharacterName{
+        Ben,
+        Jane,
+    }
+
+
+//Used to check for which characters have been completed 
     public void Start(){
         playerData = FindObjectOfType<PlayerData>(); 
         saveDataJSON = FindObjectOfType<SaveDataJSON>(); 
-        
+        switch(internalName){
+            case CharacterName.Ben:
+                if(playerData.FinishedBen)
+                    gameObject.SetActive(false);
+                break;
+            case CharacterName.Jane:
+                if(playerData.FinishedJane)
+                    gameObject.SetActive(false);
+                break;
+            default:
+            break; 
+        }
+
     }
     
     public void OnMouseDown(){
