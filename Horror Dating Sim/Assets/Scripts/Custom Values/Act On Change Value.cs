740b09cd02d1b4fb72a12db55c94b8a81c5dbfc2
William <wkmin1@uci.edu>
2025-04-24 16:47:01 -0700

    Fixed up ActOnChangedValue's UnityEvent

--- Horror Dating Sim/Assets/Scripts/Custom Values/Act On Change Value.cs
+++ Horror Dating Sim/Assets/Scripts/Custom Values/Act On Change Value.cs
@@ -14,14 +14,13 @@ public struct ActOnChangeValue<T>
     #region Serialized Fields
 
     [SerializeField] private T _value; // 
-    [Space]
-    [SerializeField] private UnityEvent _unityEventsOnChangedValue; // 
 
     #endregion
 
     #region Private Fields
 
     private T _originalValue; // 
+    private UnityEvent _unityEventsOnChangedValue; // 
 
     #endregion
 
@@ -39,7 +38,7 @@ public struct ActOnChangeValue<T>
     /// <summary>
     /// 
     /// </summary>
-    public UnityEvent UnityEvensOnChangedValue { get => _unityEventsOnChangedValue; set => _unityEventsOnChangedValue = value; }
+    public UnityEvent UnityEventsOnChangedValue { get { if (_unityEventsOnChangedValue == null) _unityEventsOnChangedValue = new UnityEvent(); return _unityEventsOnChangedValue; } set => _unityEventsOnChangedValue = value; }
 
     /// <summary>
     /// 
@@ -60,14 +59,14 @@ public struct ActOnChangeValue<T>
     #region Public Methods
 
     /// <summary>
-    /// 
+    /// Invokes actions and events on the value
     /// </summary>
     public void ActOnValue()
     {
         if (_originalValue != null && _originalValue.Equals(_value)) return;
 
         if (ActionsOnChangedValue != null) ActionsOnChangedValue(_value);
-        _unityEventsOnChangedValue.Invoke();
+        _unityEventsOnChangedValue?.Invoke();
         _originalValue = _value;
     }
 
