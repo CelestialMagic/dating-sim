d37b4ed3aae157acdbc7223a9d57b09be605db52
William <wkmin1@uci.edu>
2025-04-25 01:51:46 -0700

    Added Sprite Functionality into Dialogue Handler

--- Horror Dating Sim/Assets/Scripts/Dialogue/Character Sprite Spawner.cs
+++ Horror Dating Sim/Assets/Scripts/Dialogue/Character Sprite Spawner.cs
@@ -1,16 +1,102 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
+/// <summary>
+/// 
+/// 
+/// Author: William Min
+/// </summary>
 public class CharacterSpriteSpawner : MonoBehaviour
 {
-    [SerializeField] private Transform _spriteParent;
-    [SerializeField] private CharacterSprite _spritePrefab;
-    [SerializeField] private ObjectPool<CharacterSprite> _pool;
+    #region Serialized Fields
 
-    private CharacterSprite CreateSprite()
+    [SerializeField] private CharacterSprite _spritePrefab; // 
+    [SerializeField] private ObjectPool<CharacterSprite> _pool; // 
+
+    #endregion
+
+    #region Private Fields
+
+    private List<CharacterSprite> _activeSprites; // 
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public List<CharacterSprite> ActiveSprites { get => _activeSprites; }
+
+    #endregion
+
+    #region Monobehavior Callbacks
+
+    // Sets up spawner
+    private void Awake()
     {
-        CharacterSprite sprite = Instantiate(_spritePrefab);
-        
+        _activeSprites = new List<CharacterSprite>();
+        _pool = new ObjectPool<CharacterSprite>(_createSprite, _onTakeSpriteFromPool, _onReturnSpriteFromPool, _onDestroySprite, true, 4, 8);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="parent"></param>
+    public void CreateSprite(Transform parent)
+    {
+        Transform spriteTransform = _pool.Get().transform;
+        spriteTransform.SetParent(parent);
+        spriteTransform.localPosition = Vector3.zero;
+        spriteTransform.localRotation = Quaternion.identity;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sprite"></param>
+    public void ReleaseSprite(CharacterSprite sprite)
+    {
+        _pool.Release(sprite);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    // Creates a new instance of a character sprite
+    private CharacterSprite _createSprite()
+    {
+        CharacterSprite sprite = Instantiate(_spritePrefab, Vector3.zero, Quaternion.identity);
+        sprite.SetPool(_pool);
         return sprite;
     }
+
+    // 
+    private void _onTakeSpriteFromPool(CharacterSprite sprite)
+    {
+        _activeSprites.Add(sprite);
+        sprite.gameObject.SetActive(true);
+    }
+
+    // 
+    private void _onReturnSpriteFromPool(CharacterSprite sprite)
+    {
+        _activeSprites.Remove(sprite);
+        sprite.gameObject.SetActive(false);
+    }
+
+    // 
+    private void _onDestroySprite(CharacterSprite sprite)
+    {
+        _activeSprites.Remove(sprite);
+        Destroy(sprite.gameObject);
+    }
+
+    #endregion
 }
