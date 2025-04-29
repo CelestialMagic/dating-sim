3330a6ed71e6d29b92690b23a2ad0f8968565ef5
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-04-29 10:13:38 -0700

    Added Title Card Fading

---
+++ Horror Dating Sim/Assets/Scripts/TitleCardScripts/TitleCard.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TitleCard : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup panel; 
+
+    [SerializeField]
+    private DialogueHandler dialogueHandler; 
+
+    [SerializeField]
+    private float startingAlpha, endingAlpha, fadeRate;
+
+    private bool startFade = false; 
+
+//Sets the FadeOnClick() value to true
+    public void FadeOnClick(){
+        startFade = true;
+    }
+
+//FadeAway() is called using Update
+    private void Update(){
+        FadeAway(); 
+    }
+
+//FadeAway() slowly changes the alpha value of the canvas group 
+   private void FadeAway(){
+        if(startFade){
+            startingAlpha = Mathf.MoveTowards(startingAlpha, endingAlpha, fadeRate * Time.deltaTime);
+            panel.alpha = startingAlpha;
+            if(startingAlpha == endingAlpha){
+                panel.gameObject.SetActive(false);
+                dialogueHandler.ProceedWithText(); 
+                startFade = false; 
+            }
+            
+
+        }
+
+   }
+}
