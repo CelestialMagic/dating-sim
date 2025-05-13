7a4c40918d8395f84b75ff99c93469b2dc578cf8
William <wkmin1@uci.edu>
2025-05-13 03:32:54 -0700

    Reworked Sprite Search, Sprite Settings, and Speaker Properties

--- Horror Dating Sim/Assets/Scripts/Character Sprite/Sprite Search.cs
+++ Horror Dating Sim/Assets/Scripts/Character Sprite/Sprite Search.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Parameters for searching and returning a sprite.
@@ -10,22 +11,22 @@ public struct SpriteSearch
 {
     #region Serialized Fields
 
-    [SerializeField] private CharacterImage.ImageType _imageType; // Type of sprite to search for
-    [SerializeField] private int _version; // Version number of the type of sprite to searh for
+    [SerializeField] private CharacterProfile _profile;             // Character profile to get images from
+    [SerializeField] private CharacterImage.ImageType _imageType;   // Type of sprite to search for
+    [SerializeField] private int _version;                          // Version number of the type of sprite to searh for
 
     #endregion
 
     #region Properties
 
-    /// <summary>
-    /// Returns the tyep of the searched sprite.
-    /// </summary>
-    public CharacterImage.ImageType ImageType { get => _imageType; }
-
-    /// <summary>
-    /// Returns the version number of the type of the searched sprite.
-    /// </summary>
-    public int Version { get => _version; }
-
     #endregion
+
+    /// <summary>
+    /// Changes the image's sprite according to the sprite search.
+    /// </summary>
+    /// <param name="spriteImage">Image reference to change sprite</param>
+    public void ChangeImage(Image spriteImage)
+    {
+        spriteImage.sprite = _profile.GetSprite(_imageType, _version);
+    }
 }
--- Horror Dating Sim/Assets/Scripts/Character Sprite/Sprite Settings.cs
+++ Horror Dating Sim/Assets/Scripts/Character Sprite/Sprite Settings.cs
@@ -8,37 +8,16 @@ using UnityEngine;
 [System.Serializable]
 public struct SpriteSettings
 {
-    #region Public Fields
+    #region Serialized Fields
 
-    /// <summary>
-    /// True if the character sprite will be a subject.
-    /// </summary>
-    public ToggledField<bool> IsSubject;
-
-    /// <summary>
-    /// True if the character sprite will be revealed.
-    /// </summary>
-    public ToggledField<bool> IsRevealed;
-
-    /// <summary>
-    /// True if the character sprite will arrive onto the scene.
-    /// </summary>
-    public ToggledField<bool> HasArrived;
-
-    /// <summary>
-    /// Starting acnhor position of the image object relative to the character sprite.
-    /// </summary>
-    public ToggledField<Vector2> StartingAnchorPosition;
-
-    /// <summary>
-    /// Starting dimensions of the image object relative to the character sprite.
-    /// </summary>
-    public ToggledField<Vector2> StartingDimensions;
-
-    /// <summary>
-    /// The sprite that will be displayed on the character sprite based on sprite type and version number.
-    /// </summary>
-    public ToggledField<SpriteSearch> ImageSearchParameters;
+    [SerializeField] private int _spriteIndex;                                  // Index of sprite to modify
+    [Space]
+    [SerializeField] private ToggledField<bool> IsSubject;                      // True if the character sprite will be a subject.
+    [SerializeField] private ToggledField<bool> IsRevealed;                     // True if the character sprite will be revealed.
+    [SerializeField] private ToggledField<bool> HasArrived;                     // True if the character sprite will arrive onto the scene.
+    [SerializeField] private ToggledField<Vector2> StartingAnchorPosition;      // Starting acnhor position of the image object relative to the character sprite.
+    [SerializeField] private ToggledField<Vector2> StartingDimensions;          // Starting dimensions of the image object relative to the character sprite.
+    [SerializeField] private ToggledField<SpriteSearch> ImageSearchParameters;  // The sprite that will be displayed on the character sprite based on sprite type and version number.
 
     #endregion
 
@@ -47,14 +26,19 @@ public struct SpriteSettings
     /// <summary>
     /// Updates the given sprite with its parameters.
     /// </summary>
-    /// <param name="sprite">The character sprite to be updated</param>
-    public void UpdateCharacterSprite(CharacterSprite sprite)
+    /// <param name="sprites">character sprite list to modify</param>
+    public void UpdateCharacterSprite(CharacterSprite[] sprites)
     {
+        if (_spriteIndex < 0 || _spriteIndex >= sprites.Length)
+            return;
+
+        CharacterSprite sprite = sprites[_spriteIndex];
+
         if (HasArrived.IsEnabled) sprite.HasArrived = HasArrived.Value;
         if (IsRevealed.IsEnabled) sprite.IsRevealed = IsRevealed.Value;
         if (IsSubject.IsEnabled) sprite.IsSubject = IsSubject.Value;
         if (StartingAnchorPosition.IsEnabled) sprite.SpritePosition = StartingAnchorPosition.Value;
-        if (StartingDimensions.IsEnabled) sprite.SpriteScale = StartingDimensions.Value;
+        if (StartingDimensions.IsEnabled) sprite.SpriteDimensions = StartingDimensions.Value;
         if (ImageSearchParameters.IsEnabled) sprite.ChangeImage(ImageSearchParameters.Value);
     }
 
