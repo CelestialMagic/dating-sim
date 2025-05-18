777af4c58168cbf095aaf00b60eaea8c137dc971
William <wkmin1@uci.edu>
2025-05-17 17:46:51 -0700

    Updated Dialogue Handler

--- Horror Dating Sim/Assets/Scripts/UI/Dialogue Display.cs
+++ Horror Dating Sim/Assets/Scripts/UI/Dialogue Display.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 /// <summary>
@@ -13,12 +12,6 @@ public class DialogueDisplay : MonoBehaviour
     [SerializeField] private TMP_Text _nameText;        // Reference to text box containing current speaker name
     [SerializeField] private GameObject _proceedHolder; // 
     [SerializeField] private GameObject _choiceHolder;  // 
-    [Space]
-    [SerializeField] private int _spriteCount = 5;                              // 
-    [SerializeField] private SpawnerWithParent<CharacterSprite> _spriteSpawner; //
-    [Space]
-    [SerializeField] private int _buttonCount = 30;                             // 
-    [SerializeField] private SpawnerWithParent<Button> _buttonSpawner;          // 
 
     /// <summary>
     /// 
@@ -30,25 +23,15 @@ public class DialogueDisplay : MonoBehaviour
     /// </summary>
     public string NameText { get => _nameText.text; set => _nameText.text = value; }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public SpawnerWithParent<CharacterSprite> SpriteSpawner { get => _spriteSpawner; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public SpawnerWithParent<Button> ButtonSpawner { get => _buttonSpawner; }
-
     //
     private void Awake()
     {
-        DialogueHandler handler = GameObject.FindObjectOfType<DialogueHandler>();
+        DialogueHandler handler = FindObjectOfType<DialogueHandler>();
 
         handler.OnChangeDialogue += s => _dialogueText.text = s;
         handler.OnChangeName += s => _nameText.text = s;
-        handler.OnProcessScript.AddListener(() => _spriteSpawner.SetSpawnedCount(_spriteCount));
-        handler.OnProcessScript.AddListener(() => _buttonSpawner.SetSpawnedCount(_buttonCount));
+        handler.OnSkipDialogue.AddListener(() => ToggleChoiceHolder(false));
+        handler.OnBeginLine.AddListener(() => ToggleProceedHolder(true));
     }
 
     /// <summary>
