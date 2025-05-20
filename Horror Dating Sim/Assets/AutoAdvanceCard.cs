5762a590389a45ea21ce420f55af323cafa8b87a
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-05-20 12:06:00 -0700

    Imported More of Jane Act 2

---
+++ Horror Dating Sim/Assets/AutoAdvanceCard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAdvanceCard : TitleCard
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        startFade = true; 
+    }
+
+}
